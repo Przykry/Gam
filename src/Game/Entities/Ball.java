0958762994fb36414dc66bc6f30e6ed23bcabd26
Przykry <krzysiek6991@gmail.com>
2017-05-02 13:55:46 +0200

    Bouncing balls, thread ball

--- src/Game/Entities/Ball.java
+++ src/Game/Entities/Ball.java
@@ -1,10 +1,13 @@
 package Game.Entities;
 
+import Game.Windows.WindowInt;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.Callable;
 
 /**
  * Created by Daniel on 28.04.2017.
@@ -16,12 +19,14 @@ public class Ball {
     private int centerX, centerY;
     private int radius;
     private Image ballImage;
+    JPanel observer;
 
     public Ball(int x, int y, int speedX, int speedY, JPanel observer){
         this.x = x;
         this.y = y;
         this.speedX = speedX;
         this.speedY = speedY;
+        this.observer = observer;
         setBallImage();
         setRadius(ballImage,observer);
         this.width = this.heigth = this.radius*2;
@@ -122,6 +127,10 @@ public class Ball {
         this.speedY = -this.speedY;
     }
 
+
+
+
+
     public void increaseSpeedX(int x){
         if(this.speedX >= 0){
             this.speedX += x;
@@ -157,5 +166,66 @@ public class Ball {
             setSpeedY(this.speedY + y);
         }
     }
+
+
+    public void directoryOfBall() {
+        ballHittingBorder(observer.getHeight(),observer.getWidth());
+        move();
+    }
+
+    public void drawBall(Graphics g){
+        g.drawImage(getBallImage(), getX(), getY(), observer);
+    }
+
+    public void ballHittingBorder(int heigth, int width) {
+        if (new Rectangle(0, 0, 15, heigth).intersects(new Rectangle(this.getX(), this.getY(), this.getRadius() * 2, this.getRadius() * 2))) {
+            this.reverseSpeedX();
+            if (this.getX() < 15) this.setX(16);
+        }
+        if (new Rectangle(0, 0, width, 15).intersects(new Rectangle(this.getX(), this.getY(), this.getRadius() * 2, this.getRadius() * 2))) {
+            this.reverseSpeedY();
+
+            if (this.getY() < 15) this.setY(16);
+        }
+        if (new Rectangle(0, heigth - 15, width, 15).intersects(new Rectangle(this.getX(), this.getY(), this.getRadius() * 2, this.getRadius() * 2))) {
+            this.reverseSpeedY();
+            if (this.getY() + this.getHeigth() > heigth - 15) this.setY(heigth - 1 - 15 - this.getHeigth());
+        }
+        if (new Rectangle(width - 15, 0, 15, heigth).intersects(new Rectangle(this.getX(), this.getY(), this.getRadius() * 2, this.getRadius() * 2))) {
+            this.reverseSpeedX();
+            if (this.getX() + this.getWidth() > width - 15) this.setX(width - 1 - 15 - this.getWidth());
+        }
+    }
+
+    public double calculatePythagoras(Ball ball1) {
+        double aplusb = Math.pow(ball1.getCenterX() - this.getCenterX(), 2) + Math.pow(this.getCenterY() - ball1.getCenterY(), 2);
+        double toReturn = Math.sqrt(aplusb);
+        return toReturn;
+    }
+
+    public void calculateDirection(Ball ball1) {
+        double xDir1 = (ball1.getCenterX() - this.getCenterX()) / calculatePythagoras(ball1);
+        double yDir1 = (this.getCenterY() - ball1.getCenterY()) / calculatePythagoras(ball1);
+
+        double xDir2 = (this.getCenterX() - ball1.getCenterX()) / calculatePythagoras(ball1);
+        double yDir2 = (ball1.getCenterY() - this.getCenterY()) / calculatePythagoras(ball1);
+
+        int dirX1 = (int) Math.round(xDir1 * 40);
+        int dirY1 = (int) Math.round(yDir1 * 40);
+
+        int dirX2 = (int) Math.round(xDir2 * 40);
+        int dirY2 = (int) Math.round(yDir2 * 40);
+        ball1.setSpeedX(dirX1);
+        ball1.setSpeedY(-dirY1);
+        this.setSpeedX(dirX2);
+        this.setSpeedY(-dirY2);
+    }
+
+    public void checkIfIntersects(Ball ball1) {
+        if (calculatePythagoras(ball1) <= ball1.getRadius() + this.getRadius()) {
+            calculateDirection(ball1);
+        }
+    }
+
 }
 
--- src/Game/Windows/MainWindow.java
+++ src/Game/Windows/MainWindow.java
@@ -1,5 +1,6 @@
 package Game.Windows;
 
+import Game.BouncingBall;
 import Game.Entities.Ball;
 import Game.ButtonListeners.ExitGameButtonListener;
 import Game.ButtonListeners.HallOfFameButtonListener;
@@ -7,10 +8,13 @@ import Game.ButtonListeners.MenuButtonListener;
 import Game.ButtonListeners.StartGameButtonListener;
 
 import javax.swing.*;
+import javax.swing.Timer;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.*;
+import java.util.List;
 
 /**
  * Created by Daniel on 24.04.2017.
@@ -38,9 +42,9 @@ public class MainWindow extends JPanel implements ActionListener, WindowInt {
             new ExitGameButtonListener()
     };
     Image mainBackground = null;
-
     Timer timer = new Timer(8, this);
-
+    Thread thread[] = new Thread[10];
+    List<Ball> ballList = new ArrayList<>();
     public MainWindow(int width, int heigth){
         this.width = width;
         this.heigth = heigth;
@@ -50,8 +54,7 @@ public class MainWindow extends JPanel implements ActionListener, WindowInt {
         catch(IOException e){
             e.printStackTrace();
         }
-        this.ball1 = new Ball(0, 0, 50, 50, this);
-        this.ball2 = new Ball(width - 60, 0, -40, 40, this);
+        addBouncigBalls(12);
         for (int i = 0; i < 4; i++) {
             createButton(buttons[i], width - 300, i * 60 + i * 40 + 60, filePaths[i], listeners[i]);
             this.add(buttons[i]);
@@ -61,75 +64,34 @@ public class MainWindow extends JPanel implements ActionListener, WindowInt {
         timer.start();
     }
 
+    private void addBouncigBalls(int numberOfBalls){
+        for(int i=0;i<10;i++){
+            ballList.add(new Ball(
+                        new Random().nextInt(850),
+                        new Random().nextInt(630),
+                        30,
+                        20,
+                        this)
+                        );
+        }
+
+        List<Runnable> bouncingBall = new ArrayList<>();
+        for(int i=0;i<10;i++) {
+            bouncingBall.add(new BouncingBall(ballList,i,this));
+            thread[i] = new Thread(bouncingBall.get(i));
+            thread[i].start();
+        }
+    }
+
     public void paintComponent(Graphics graphics) {
         //background
         drawBackground(graphics, mainBackground, this);
         drawBorders(graphics,width,heigth);
-
-        graphics.drawImage(ball1.getBallImage(), ball1.getX(), ball1.getY(), this);
-        graphics.drawImage(ball2.getBallImage(), ball2.getX(), ball2.getY(), this);
+        for(Ball ball : ballList) ball.drawBall(graphics);
     }
 
-    public void directoryOfBall() {
-        ball1.move();
-        ball2.move();
-        ballHittingBorder(ball1);
-        ballHittingBorder(ball2);
-    }
-
-    public void ballHittingBorder(Ball ball) {
-        if (new Rectangle(0, 0, 15, heigth).intersects(new Rectangle(ball.getX(), ball.getY(), ball.getRadius() * 2, ball.getRadius() * 2))) {
-            ball.reverseSpeedX();
-            if (ball.getX() < 15) ball.setX(16);
-        }
-        if (new Rectangle(0, 0, width, 15).intersects(new Rectangle(ball.getX(), ball.getY(), ball.getRadius() * 2, ball.getRadius() * 2))) {
-            ball.reverseSpeedY();
-            if (ball.getY() < 15) ball.setY(16);
-        }
-        if (new Rectangle(0, heigth - 15, width, 15).intersects(new Rectangle(ball.getX(), ball.getY(), ball.getRadius() * 2, ball.getRadius() * 2))) {
-            ball.reverseSpeedY();
-            if (ball.getY() + ball.getHeigth() > heigth - 15) ball.setY(heigth - 1 - 15 - ball.getHeigth());
-        }
-        if (new Rectangle(width - 15, 0, 15, heigth).intersects(new Rectangle(ball.getX(), ball.getY(), ball.getRadius() * 2, ball.getRadius() * 2))) {
-            ball.reverseSpeedX();
-            if (ball.getX() + ball.getWidth() > width - 15) ball.setX(width - 1 - 15 - ball.getWidth());
-        }
-    }
-
-    public double calculatePythagoras() {
-        double aplusb = Math.pow(ball1.getCenterX() - ball2.getCenterX(), 2) + Math.pow(ball2.getCenterY() - ball1.getCenterY(), 2);
-        double toReturn = Math.sqrt(aplusb);
-        return toReturn;
-    }
-
-    public void calculateDirection() {
-        double xDir1 = (ball1.getCenterX() - ball2.getCenterX()) / calculatePythagoras();
-        double yDir1 = (ball2.getCenterY() - ball1.getCenterY()) / calculatePythagoras();
-
-        double xDir2 = (ball2.getCenterX() - ball1.getCenterX()) / calculatePythagoras();
-        double yDir2 = (ball1.getCenterY() - ball2.getCenterY()) / calculatePythagoras();
-
-        int dirX1 = (int) Math.round(xDir1 * 70);
-        int dirY1 = (int) Math.round(yDir1 * 70);
-
-        int dirX2 = (int) Math.round(xDir2 * 70);
-        int dirY2 = (int) Math.round(yDir2 * 70);
-        ball1.setSpeedX(dirX1);
-        ball1.setSpeedY(-dirY1);
-        ball2.setSpeedX(dirX2);
-        ball2.setSpeedY(-dirY2);
-    }
-
-    public void checkIfIntersects() {
-        if (calculatePythagoras() <= ball1.getRadius() + ball2.getRadius()) {
-            calculateDirection();
-        }
-    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        directoryOfBall();
-        checkIfIntersects();
-        this.repaint();
     }
 }
