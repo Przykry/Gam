7e0b5d598c94b6757a25cb6ffe431438fdbd9d36
Daniel <danielserszen@gmail.com>
2017-05-19 22:50:37 +0200

    Lewandowski is in the game!

--- src/Game/Windows/ChoosePlayerWindow.java
+++ src/Game/Windows/ChoosePlayerWindow.java
@@ -21,12 +21,14 @@ public class ChoosePlayerWindow extends JPanel implements WindowInt {
 
     public Player[] player1Models = new Player[]{
             new Player(50,50,"Messi",10,7,55),
-            new Player(50,50,"Kuchy",8,5,85)
+            new Player(50,50,"Kuchy",8,5,85),
+            new Player(50,50,"Lewy",10,6,90)
     };
 
     public Player[] player2Models = new Player[]{
             new Player(50,50,"Messi",10,7,55),
             new Player(50,50,"Kuchy",8,5,85),
+            new Player(50,50,"Lewy",10,6,90)
     };
 
     private static Player player1;
